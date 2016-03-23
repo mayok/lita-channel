6b3777b864ac41c33e8ba41be15eab1fad91e2a6
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-03-23 18:26:47 +0900

    Update channel.rb

--- lib/lita/extensions/channel.rb
+++ lib/lita/extensions/channel.rb
@@ -6,10 +6,12 @@ module Lita
         route   = payload[:route]
         message = payload[:message]
 
-        if message.source.room == Lita::Room.find_by_name(route.extensions[:channel]).id
-          return true
-        else
-          return false
+        if Lita::Room.find_by_name(route.extensions[:channel]).respond_to? :id
+          if message.source.room == Lita::Room.find_by_name(route.extensions[:channel]).id
+            return true
+          else
+            return false
+          end
         end
       end
 
